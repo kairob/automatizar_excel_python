bc1dfb9f2bc9bb21cde41d2347b30f508a8ed9cd
kairob <64109098+kairob@users.noreply.github.com>
2025-06-02 22:11:18 -0300

    0206

--- robo.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import Workbook, load_workbook
-
-
-grade= load_workbook('grade.xlsx')
-grade_003  = grade['grade003']
-
-for row in grade_003.iter_rows(values_only=True):
-    print(row)
